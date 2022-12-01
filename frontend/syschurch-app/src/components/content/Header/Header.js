9d79a0cd23fa268d434f78ed58eac32d9ca60406
GilmarFreire <93558838+GilmarFreire@users.noreply.github.com>
2022-12-01 00:57:21 -0300

    Inclusão de dependências e Components
    Inclusão das dependências
    
    npm install react-bootstrap
    npm install react-icons --save
    
    Criação de component Search com icone de lupa do react-icons

--- frontend/syschurch-app/src/components/content/Header/Header.js
+++ frontend/syschurch-app/src/components/content/Header/Header.js
@@ -1,10 +1,22 @@
 import'./Header.css'
+import Search from './Search';
 
 function Header() {
     return(
         <header className="container-header">
-            <h1>ASSEMBLÉIA DE DEUS</h1>
-            <h3>MIN. SÃO GOTARDO-MG</h3>      
+            <h1>
+                ASSEMBLÉIA DE DEUS
+            </h1>
+            <h3>
+                MIN. SÃO GOTARDO-MG
+            </h3>
+            <Search />
+
+
+            
+
+
+
         </header>
     )
 }
