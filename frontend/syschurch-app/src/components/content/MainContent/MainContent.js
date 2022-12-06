5d67ab2083b33c520b213a80d159ef0d2a5f0356
GilmarFreire <93558838+GilmarFreire@users.noreply.github.com>
2022-12-06 03:42:35 -0300

    created content MainContent.js
    created content MainContent.js

--- frontend/syschurch-app/src/components/content/MainContent/MainContent.js
+++ frontend/syschurch-app/src/components/content/MainContent/MainContent.js
@@ -1,7 +1,36 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import "./MainContent.css"
+
 function MainContent() {
     return(
-        <div>
-            MainContent
+        <div className="container-mainContent">
+           <Container>  
+                <h5 className='h6-MainContent'>Acesso Rápido</h5>   
+                <Row>
+                    <Col className="col-MainContent" sm>
+                        <button className='bottom-MainContent'>Membros</button>
+                    </Col>
+                    <Col className="col-MainContent" sm>
+                        <button className='bottom-MainContent'>Relatórios</button>
+                    </Col>
+                    <Col className="col-MainContent" sm>
+                        <button className='bottom-MainContent'>Eventos</button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="col-MainContent" sm>
+                        <button className='bottom-MainContent'>Cartas</button>
+                    </Col>
+                    <Col className="col-MainContent" sm>
+                        <button className='bottom-MainContent'>Contas a Pagar</button>
+                    </Col>
+                    <Col className="col-MainContent" sm>
+                        <button className='bottom-MainContent'>Inventário</button>
+                    </Col>
+                </Row>      
+            </Container>
         </div>
     )
 }
